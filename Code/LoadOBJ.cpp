a71f3310da9931f27792f3c304187fca0c1809f9
isaaccalvis <isaaccalvis@gmail.com>
2017-12-31 13:00:53 +0100

    some

--- Code/LoadOBJ.cpp
+++ Code/LoadOBJ.cpp
@@ -37,8 +37,9 @@ bool loadOBJ( const char * path, std::vector < vec3 > & out_vertices, std::vecto
 		else if (strcmp(lineHeader, "f") == 0) {
 			std::string vertex1, vertex2, vertex3;
 			unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-			int matches = fscanf(file, "%d//%d//%d %d//%d//%d %d//%d//%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-			if (matches != 9) {
+			//int matches = fscanf(file, "%d//%d//%d %d//%d//%d %d//%d//%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+			int matches = fscanf(file, "%d//%d %d//%d %d//%d\n", &vertexIndex[0], &normalIndex[0], &vertexIndex[1], &normalIndex[1], &vertexIndex[2], &normalIndex[2]);
+			if (matches != 6) {
 				printf("File can't be read by our simple parser : ( Try exporting with other options\n");
 				return false;
 			}
